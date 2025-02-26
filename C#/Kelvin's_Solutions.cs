e7e2db25101a0c6da4a2a167252af75e950208e3
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2025-02-26 08:59:48 +0300

    Exercise 2 Kelvin's solution

--- C#/Kelvin's_Solutions.cs
+++ C#/Kelvin's_Solutions.cs
@@ -23,3 +23,15 @@ public class Exercise5
     }
 }
 
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine(-1 + 4 * 6);
+        System.Console.WriteLine((35+ 5 ) % 7);
+        System.Console.WriteLine(14 + -4 * 6 / 11);
+        System.Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+    }
+}
