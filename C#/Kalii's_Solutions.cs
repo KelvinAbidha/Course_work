a271fa4b81373140e9af54f509982eeddbe232fb
KALIIKARDZ <145854310+KALIIKARDZ@users.noreply.github.com>
2025-02-27 22:17:53 +0300

    Update Kalii's_Solutions.cs

--- C#/Kalii's_Solutions.cs
+++ C#/Kalii's_Solutions.cs
@@ -27,3 +27,17 @@ public class Exercise5
         Console.Read();  // Keeping the console window open until a key is pressed
     }
 }
+
+*/ Question 2:
+//Using Visual studio arithmetic opertarions are performed automaticallly
+class Arithmatics
+{
+    static void Main()
+    {
+        Console.WriteLine(-1 + 4 * 6);
+        Console.WriteLine( (35 + 5) % 7);
+        Console.WriteLine( 14 + -4 * 6 / 11);
+        Console.WriteLine(  2 + 15/6 * 1 -7 % 2);
+
+    }
+}
