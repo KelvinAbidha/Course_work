85e9613bd2dba2db28ef898688b0d11c2e39fb6f
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2025-03-01 11:10:52 +0300

    Exercise 2 solution

--- Java/Kelvin's_Solutions.java
+++ Java/Kelvin's_Solutions.java
@@ -6,3 +6,17 @@ class Main {
         System.out.println(name);
     }
 }
+//Exercise 2
+class Main{
+    public static void main(String[]args)
+    {
+        int number_1 = 4;
+        int num_2 = 8;
+        System.out.println("Number 1 =" + number_1);
+        System.out.println("Number 2 =" + num_2);
+        int num_3 = num_2;
+        num_2 = number_1;
+        number_1 = num_3;
+        System.out.print("Number 1, Number 2 " +number_1+num_2);
+    }
+}
