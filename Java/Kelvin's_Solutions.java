541821197d6d0e9a686a5a3745c5bbf1bb43a8c7
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2025-03-01 07:21:40 +0300

    Update Kelvin's_Solutions.java

--- Java/Kelvin's_Solutions.java
+++ Java/Kelvin's_Solutions.java
@@ -1,1 +1,8 @@
-
+//Exercise 1 
+class Main {
+    public static void main(String[] args) {
+        String name = "Kelvin";
+        System.out.println("Hello");
+        System.out.println(name);
+    }
+}
