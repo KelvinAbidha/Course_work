d28dc783ba36b5385cbfc28ccd45417af995f9d4
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2025-03-04 16:24:50 +0300

    Update Kelvin's_Solutions.java

--- Java/Kelvin's_Solutions.java
+++ Java/Kelvin's_Solutions.java
@@ -20,3 +20,26 @@ class Main{
         System.out.print("Number 1, Number 2 " +number_1+num_2);
     }
 }
+
+//Exercise 3 
+import java.util.Scanner;
+
+class Exercise_3
+{
+    public static void main(String [] args)   
+    {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter num1 =");
+        int num1 = in.nextInt();
+        System.out.print("Enter num2 =");
+        int num2 = in.nextInt();
+        
+        int product = num1*num2;
+        System.out.print("Product = "+product);
+    }
+    
+}
+
+//Exercise 4
+
