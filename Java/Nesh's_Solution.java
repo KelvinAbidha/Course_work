2f87c168a26a0659c7f11affb043d179973444fd
NESH <146263730+nesh-hue@users.noreply.github.com>
2025-03-09 13:05:10 +0300

    Update Nesh's_Solution.java

--- Java/Nesh's_Solution.java
+++ Java/Nesh's_Solution.java
@@ -1,5 +1,9 @@
-public class HelloWorld{
+Question 1///
+    class Main{
     public static void main(String[]args){
-        System.out.println("Hello World!");
-        String myName = "Nesh";
-        System.out.println("My Name is "+ myName);
+        System.out.println("Hello");
+        System.out.println("My name is Nesh");}
+}
+Question 2///
+    
+    
