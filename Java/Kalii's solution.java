3105b6e7e11e368af35a942acb3b35dffba41224
KALIIKARDZ <145854310+KALIIKARDZ@users.noreply.github.com>
2025-03-09 18:54:07 +0300

    Update Kalii's solution.java

--- Java/Kalii's solution.java
+++ Java/Kalii's solution.java
@@ -12,3 +12,30 @@ class Main {
     myObj.close(); // Close Scanner to prevent resource leaks
   }
 }
+
+// Question 2
+// Java program to Swap
+// Two variables
+
+// Driver Class
+public class SWAP {
+    // main function
+    public static void main(String[] args)
+    {
+        int x = 100, y = 200;
+
+        System.out.println("Before Swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        // Swapping using three
+        // Variables
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("After swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
