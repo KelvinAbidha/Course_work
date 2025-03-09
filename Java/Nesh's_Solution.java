f370ef89d531fd34459eab7daa9cc99eb4467ffd
NESH <146263730+nesh-hue@users.noreply.github.com>
2025-03-09 13:51:13 +0300

    Update Nesh's_Solution.java

--- Java/Nesh's_Solution.java
+++ Java/Nesh's_Solution.java
@@ -45,4 +45,14 @@ public class Main{
 }
     
  Question 4///
+public class Main{
+    public static void main(String[]args){
+        System.out.println("The following are the first 10 natural numbers:");
+        for(int i=1;i<=10;i++) {
+            System.out.println(i);
+        }   
+    }  
+}
      
+Question 4///
+    
